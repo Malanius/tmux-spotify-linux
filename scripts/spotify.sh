b4b0ab40200ee38e1d0b96d667fc12179f05d3c2
Malanius Privierre <malaniusprivierre@gmail.com>
2024-01-05 17:51:27 +0100

    refactor: extract not running menu to function

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -22,16 +22,22 @@ get_loop_status() {
     /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player LoopStatus | jq -r '.data')
 }
 
+show_not_running_menu() {
+  tmux display-menu -T "#[align=centre fg=green] Spotify " -x R -y P \
+    "Spotify is not runnning!" "" "" \
+    \
+    "" \
+    "Close menu" q ""
+  # "Open Spotify" o "run -b 'source \"$CURRENT_DIR/spotify.sh\" && open_spotify'" \
+}
+
 show_menu() {
   local spotify_pid=$(pidof -s spotify || pidof -s .spotify.wrapped)
   if [ -z "$spotify_pid" ]; then
-    $(
-      tmux display-menu -T "#[align=centre fg=green]Spotify" -x R -y P \
-        "Spotify is not runnning!" "" "" \
-        \
-        "" \
-        "Close menu" q "" # "Open Spotify" o "run -b 'source \"$CURRENT_DIR/spotify.sh\" && open_spotify'" \
-    )
+    show_not_running_menu
+    return
+  fi
+
   # elif [[ $id == *":episode:"* ]]; then
   # TODO: handle this after songs are working correctly
   #   $(tmux display-menu -T "#[align=centre fg=green]Spotify" -x R -y P \
@@ -49,51 +55,49 @@ show_menu() {
   #       "" \
   #       "Close menu"       q "" \
   #   )
+  local metadata=$(get_metadata)
+  local artist=$(echo $metadata | jq -r '.data["xesam:artist"].data[0]') # Spotify only sends the first artist anywas
+  local track_name=$(echo $metadata | jq -r '.data["xesam:title"].data')
+  local album=$(echo $metadata | jq -r '.data["xesam:album"].data')
+  local id=$(echo $metadata | jq -r '.data["mpris:trackid"].data')
+  local track_url=$(echo $metadata | jq -r '.data["xesam:url"].data')
+
+  local is_shuffle_on=$(get_shuffle_status)
+  local shuffling_label=""
+  if [ "$is_shuffle_on" == "true" ]; then
+    shuffling_label="Turn off shuffle"
   else
-    local metadata=$(get_metadata)
-    local artist=$(echo $metadata | jq -r '.data["xesam:artist"].data[0]') # Spotify only sends the first artist anywas
-    local track_name=$(echo $metadata | jq -r '.data["xesam:title"].data')
-    local album=$(echo $metadata | jq -r '.data["xesam:album"].data')
-    local id=$(echo $metadata | jq -r '.data["mpris:trackid"].data')
-    local track_url=$(echo $metadata | jq -r '.data["xesam:url"].data')
-
-    local is_shuffle_on=$(get_shuffle_status)
-    local shuffling_label=""
-    if [ "$is_shuffle_on" == "true" ]; then
-      shuffling_label="Turn off shuffle"
-    else
-      shuffling_label="Turn on shuffle"
-    fi
-
-    local loop_status=$(get_loop_status)
-    local repeating_label=""
-
-    if [ "$loop_status" == "Track" ]; then
-      repeating_label="Looping track"
-    elif [ "$loop_status" == "Playlist" ]; then
-      repeating_label="Looping playlist"
-    else
-      repeating_label="Not looping"
-    fi
-
-    # "Open Spotify" o "run -b 'source \"$CURRENT_DIR/spotify.sh\" && open_spotify'" \
-    $(
-      tmux display-menu -T "#[align=centre fg=green] Spotify " -x R -y P \
-        "" \
-        "-#[nodim]Track: $track_name" "" "run -b 'printf \"%s\" $quoted_track_name | pbcopy'" \
-        "-#[nodim]Artist: $artist" "" "" \
-        "-#[nodim]Album: $album" "" "" \
-        "" \
-        "Play/Pause" p "run -b '$CURRENT_DIR/play_pause.sh'" \
-        "Next" n "run -b '$CURRENT_DIR/next_track.sh'" \
-        "Previous" b "run -b '$CURRENT_DIR/previous_track.sh" \
-        "$shuffling_label" s "run -b '$CURRENT_DIR/toggle_shuffle.sh $is_shuffle_on'" \
-        "$repeating_label" r "run -b '$CURRENT_DIR/toggle_loop.sh $loop_status'" \
-        "Copy URL" c "run -b 'echo $track_url | xclip -sel clip'" \
-        "" \
-        "Close menu" q ""
-    )
+    shuffling_label="Turn on shuffle"
   fi
+
+  local loop_status=$(get_loop_status)
+  local repeating_label=""
+
+  if [ "$loop_status" == "Track" ]; then
+    repeating_label="Looping track"
+  elif [ "$loop_status" == "Playlist" ]; then
+    repeating_label="Looping playlist"
+  else
+    repeating_label="Not looping"
+  fi
+
+  # "Open Spotify" o "run -b 'source \"$CURRENT_DIR/spotify.sh\" && open_spotify'" \
+  $(
+    tmux display-menu -T "#[align=centre fg=green] Spotify " -x R -y P \
+      "" \
+      "-#[nodim]Track: $track_name" "" "run -b 'printf \"%s\" $quoted_track_name | pbcopy'" \
+      "-#[nodim]Artist: $artist" "" "" \
+      "-#[nodim]Album: $album" "" "" \
+      "" \
+      "Play/Pause" p "run -b '$CURRENT_DIR/play_pause.sh'" \
+      "Next" n "run -b '$CURRENT_DIR/next_track.sh'" \
+      "Previous" b "run -b '$CURRENT_DIR/previous_track.sh" \
+      "$shuffling_label" s "run -b '$CURRENT_DIR/toggle_shuffle.sh $is_shuffle_on'" \
+      "$repeating_label" r "run -b '$CURRENT_DIR/toggle_loop.sh $loop_status'" \
+      "Copy URL" c "run -b 'echo $track_url | xclip -sel clip'" \
+      "" \
+      "Close menu" q ""
+  )
 }
 
 show_menu
