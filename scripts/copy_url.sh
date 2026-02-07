75be868e24aa8016d5d2804aa13fbf2f25d37bf9
Michal Slota <malaniusprivierre@gmail.com>
2026-02-07 20:56:11 +0100

    fix: use only one copy command
    It's highly unlikely there will be a system running both wl-copy and
    xclip. but this is tad bit cleaner.

--- scripts/copy_url.sh
+++ scripts/copy_url.sh
@@ -2,10 +2,11 @@
 
 track_url="$1"
 
-if command -v xclip &> /dev/null; then
-    echo "$track_url" | xclip -sel clip
-fi
-
 if command -v wl-copy &> /dev/null; then
+    echo "$track_url" | xclip -sel clip
+elif command -v xclip &> /dev/null; then
     echo "$track_url" | wl-copy
 fi
+
+# It's OK to do nothing if none of the above commands are available
+# Far better then throwing an error into tmux session
