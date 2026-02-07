1cdd85a17948c1fdb2fbae05f0ac686ccf950227
Michal Slota <malaniusprivierre@gmail.com>
2026-02-07 21:43:17 +0100

    fix: use correct copy commands

--- scripts/copy_url.sh
+++ scripts/copy_url.sh
@@ -3,9 +3,9 @@
 track_url="$1"
 
 if command -v wl-copy &> /dev/null; then
-    echo "$track_url" | xclip -sel clip
-elif command -v xclip &> /dev/null; then
     echo "$track_url" | wl-copy
+elif command -v xclip &> /dev/null; then
+    echo "$track_url" | xclip -sel clip
 fi
 
 # It's OK to do nothing if none of the above commands are available
